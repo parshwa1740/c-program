80db795eba01d78acb2db8cf8bc05816ad465db0
parshwa1740 <118458293+parshwa1740@users.noreply.github.com>
2023-01-31 14:20:02 +0530

    Add files via upload

---
+++ basic.c
@@ -0,0 +1,32 @@
+#include<stdio.h>
+main()
+{
+	printf("My name is Parshwa Patel \n");
+	printf("DOB- 17th April 2000 \n");
+	printf("AGE- 23\n");
+	printf("ADD- Bopal, Ahmedabad\n");
+	
+	int a,b,ans;
+	
+	printf("ENTER VALUE OF A ");
+	scanf("%d",&a);
+	
+	printf("ENTER VALUE OF B ");
+	scanf("%d",&b);
+	
+	ans=a+b;
+	printf("Additon :%d\n",ans);
+	
+	ans=a-b;
+	printf("Substraction :%d\n",ans);
+	
+	ans=a/b;
+	printf("Division :%d\n",ans);
+	
+	ans=a*b;
+	printf("Multiplication :%d\n",ans);
+	
+	
+	
+}
+
